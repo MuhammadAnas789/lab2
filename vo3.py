18cead68a02ff664ac584dacc8fa0612035e1ed3
namal <jawad2019@namal.edu.pk>
2021-10-03 10:29:08 +0500

    version 3

---
+++ vo3.py
@@ -0,0 +1,20 @@
+# calculator in  Python(v0.3) by Jawad
+
+# Square Root Function
+def SqRoot(num):
+	return num ** (1/2)
+
+# Main
+if __name__ == "__main__":
+	print(SqRoot(25))
+
+
+
+
+#div function by anas
+def div (num1,num2):
+       return num1/num2
+
+#main
+
+print(div(15,3))
