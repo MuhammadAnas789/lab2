2395e46f7088d9fb12c32866667495feb08578ff
MIAN ZUBAIR <zubair6986597@gmail.com>
2021-10-03 11:39:44 +0500

    Verson 5 by Mian Zubair

---
+++ v05.py
@@ -0,0 +1,14 @@
+# calculator in python ( v01 ) by Anas
+
+# calculator in python ( v05 ) by Zubair
+
+def div (num1,num2):
+       return num1/num2
+
+def mul (num1,num2):
+	return num1 * num2
+
+#main
+
+print(div(15,3))
+print(mul(3,4))
