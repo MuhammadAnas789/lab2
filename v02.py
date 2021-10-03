b32c3cfcc5eacf508dd24e821a361dec3bd6ff7f
Sharjeel-Akram <sharjeel.akram0200@gmail.com>
2021-10-03 12:31:11 +0500

    add function by sharjeel

---
+++ v02.py
@@ -0,0 +1,12 @@
+#calculator in python by anas
+
+def div (num1,num2):
+       return num1/num2
+
+
+def add(num1,num2):
+	return num1+num2
+#main
+print(div(15,3))
+print(sub(12,56))
+priny(add(12,6))
